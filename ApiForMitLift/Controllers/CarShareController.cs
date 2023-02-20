26d53b5c86a76ad78a30c8327ed52a92ef968636
Kevin Richter Krogsgaard Andersen <88718302+Keveren1@users.noreply.github.com>
2023-02-20 10:14:03 +0100

    CRUD Metoder tilføjet for account

--- ApiForMitLift/Controllers/CarShareController.cs
+++ ApiForMitLift/Controllers/CarShareController.cs
@@ -20,6 +20,24 @@ namespace ApiForMitLift.Controllers
             _dbManager = new CarDBManager(context);
 
         }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        [Route("/api/Accounts")]
+        public ActionResult<List<Account>> GetAllAccounts()
+        {
+            IEnumerable<Account> accounts = _dbManager.GetAllAccounts();
+
+            if (!accounts.Any())
+            {
+                return NotFound("No accounts here");
+            }
+            return Ok(accounts);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet]
         // GET: CarShareController
         public ActionResult<List<Car>> GetAllCars([FromQuery] DateTime? dateTimeFilter)
@@ -32,6 +50,9 @@ namespace ApiForMitLift.Controllers
             }
             return Ok(cars);
         }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
         // GET: CarShareController
         public ActionResult<Car> GetCarByID(int id)
@@ -44,12 +65,25 @@ namespace ApiForMitLift.Controllers
             }
             return Ok(car);
         }
+
+        [HttpPut]
+        [Route("/api/UpdateAccountByID/{id}")]
+        public Account PutAccount(int id, [FromBody] Account value)
+        {
+            if (value == null)
+            {
+                NotFound("no account with this id");
+            }
+            return _dbManager.UpdateAccount(id, value);
+        }
+
         // PUT api/<CarShareController>/5
         [HttpPut("{id}")]
         public Car PutCar(int id, [FromBody] Car value)
         {
             return _dbManager.UpdateCar(id, value);
         }
+
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         // DELETE api/<CarShareController>/5
@@ -62,7 +96,28 @@ namespace ApiForMitLift.Controllers
                 return NotFound("There is no cars with, ID" + id);
             }
             return Ok(result);
+        }
 
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete]
+        [Route("/api/DeleteAccount/{id}")]
+        public ActionResult<Account> DeleteAccount(int id)
+        {
+            Account account = _dbManager.GetAccountById(id);
+            if (account == null)
+            {
+                return NotFound("No account with that id " + id);
+            }
+            return _dbManager.DeleteAccount(id);
+        }
+
+
+        [HttpPost]
+        [Route("/AccountPost")]
+        public ActionResult<Account> PostAccount([FromBody] Account newAccount)
+        {
+            Account createdAccount = _dbManager.AddAccount(newAccount);
+            return Ok(createdAccount);
         }
 
     }
--- ApiForMitLift/Manager/CarDBManager.cs
+++ ApiForMitLift/Manager/CarDBManager.cs
@@ -17,15 +17,16 @@ namespace ApiForMitLift.Manager
         }
 
         //Vi henter en liste af Accounts
-        //public List<Account> GetAllAccounts()
-        //{
-        //    using (var context = _corolabContext)
-        //    {
-        //        var accounts = context.Accounts.ToList();
+        public List<Account> GetAllAccounts()
+        {
+            using (var context = _corolabContext)
+            {
+                var accounts = context.Accounts.ToList();
 
-        //        return accounts;
-        //    }
-        //}
+                return accounts;
+            }
+        }
+
         //Vi henter den fulde liste af biler.Her bruger vi using (var context) for kun at hente listen med biler
         public List<Car> GetAllCars(DateTime? dateTimeFilter)
         {
@@ -51,47 +52,48 @@ namespace ApiForMitLift.Manager
             return result.ToList();
         }
     }
-        ////Vi finder en specifik account ud fra accountets id
-        //public Account GetAccountById(int id)
-        //{
-        //    return _corolabContext.Accounts.Find(id);
-        //}
-        ////Vi finder en specifik bil ud fra bilens id
+        //Vi finder en specifik account ud fra accountets id
+        public Account GetAccountById(int id)
+        {
+            return _corolabContext.Accounts.Find(id);
+        }
+        //Vi finder en specifik bil ud fra bilens id
         public Car GetCarById(int id)
         {
             return _corolabContext.Cars.Find(id);
         }
-        ////Her tilføjer vi en ny account til vores database tabel. Værdierne til hvad Account indeholder er i vores Account.cs klasse i model folderen.
-        //public Account AddAccount(Account addAccount)
-        //{
-        //    using (var context = _corolabContext)
-        //    {
-        //        var accounts = context.Accounts.Add(addAccount);
-        //        _corolabContext.SaveChanges();
-        //        return addAccount;
-        //    }
-        //}
 
-        ////public Car AddCar(Car addCar)
-        ////{
-        ////    Account account = GetAccountById(addCar.AccountId);
-        ////    if (addCar.AccountId == account.AccountId)
-        ////    {
-        ////        _corolabContext.Add(addCar);
-        ////        _corolabContext.SaveChanges();
-        ////        return addCar;
-        ////    }
-        ////    return addCar;
-        ////}
-        ////Rental Car Delete
-        //public Account DeleteAccount(int id)
-        //{
-        //    Account account = _corolabContext.Accounts.Find(id);
-        //    // Car car = _corolabContext.Cars.Find(id);
-        //    _corolabContext.Accounts.Remove(account);
-        //    _corolabContext.SaveChanges();
-        //    return account;
-        //}
+        //Her tilføjer vi en ny account til vores database tabel. Værdierne til hvad Account indeholder er i vores Account.cs klasse i model folderen.
+        public Account AddAccount(Account addAccount)
+        {
+            using (var context = _corolabContext)
+            {
+                var accounts = context.Accounts.Add(addAccount);
+                _corolabContext.SaveChanges();
+                return addAccount;
+            }
+        }
+
+        public Car AddCar(Car addCar)
+        {
+            Account account = GetAccountById(addCar.AccountId);
+            if (addCar.AccountId == account.AccountId)
+            {
+                _corolabContext.Add(addCar);
+                _corolabContext.SaveChanges();
+                return addCar;
+            }
+            return addCar;
+        }
+        //Rental Car Delete
+        public Account DeleteAccount(int id)
+        {
+            Account account = _corolabContext.Accounts.Find(id);
+            // Car car = _corolabContext.Cars.Find(id);
+            _corolabContext.Accounts.Remove(account);
+            _corolabContext.SaveChanges();
+            return account;
+        }
         public Car DeleteCar(int id)
         {
             Car car = _corolabContext.Cars.Find(id);
@@ -101,17 +103,17 @@ namespace ApiForMitLift.Manager
             return car;
         }
 
-        //public Account UpdateAccount(int id, Account updates)
-        //{
-        //    Account account = _corolabContext.Accounts.Find(id);
-        //    account.UserName = updates.UserName;
-        //    account.DateOfBirth = updates.DateOfBirth;
-        //    account.UserAddress = updates.UserAddress;
-        //    account.Phone = updates.Phone;
-        //    account.Email = updates.Email;
-        //    _corolabContext.SaveChanges();
-        //    return account;
-        //}
+        public Account UpdateAccount(int id, Account updates)
+        {
+            Account account = _corolabContext.Accounts.Find(id);
+            account.UserName = updates.UserName;
+            account.DateOfBirth = updates.DateOfBirth;
+            account.UserAddress = updates.UserAddress;
+            account.Phone = updates.Phone;
+            account.Email = updates.Email;
+            _corolabContext.SaveChanges();
+            return account;
+        }
         ////Vi opdaterer Car og dens værdier, vi kan ikke opdatere Car.Id, da det er primary key. Det samme gælder for Account.Id, da en primary key skal være unik.
         public Car UpdateCar(int id, Car updates)
         {
